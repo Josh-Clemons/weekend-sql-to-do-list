89ca923670217f29cd2639b7a3b6908f3942e863
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-11 15:44:26 -0600

    GET working properly

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,62 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery loaded');
+    getTasks();
+    clickListeners();
 }
+
+function clickListeners() {
+    $('#submitButton').on('click', postTask);
+};
+
+
+// GET tasks from database
+function getTasks() {
+    console.log('in getTasks');
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then(function(response) {
+        renderTable(response.rows);
+    }).catch(function(error) {
+        alert('error GETting tasks', error);
+    });
+};
+
+function postTask() {
+    // console.log('in postTask');
+    let newTask = {
+        owner: $('#ownerInput').val(),
+        date: $('#dateInput').val(),
+        details: $('#taskDescriptionInput').val()
+    }
+
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: newTask
+    }).then(function(response) {
+        console.log('post success response', response);
+    }).catch(function (error) {
+        alert('error POSTing', error);
+    });
+};
+
+
+
+function renderTable(tasks) {
+    $('#taskTableBody').empty();
+
+    for (let task of tasks) {
+        $('#taskTableBody').append(`
+            <tr>
+                <td>${task.owner}</td>
+                <td>${task.date}</td>
+                <td>${task.details}</td>
+                <td>${task.is_complete}</td>
+                <td><button>Complete</button></td>
+                <td><button>Delete</button></td>
+            </tr>
+        `);
+    };
+};
--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -1,15 +1,17 @@
 const { Router } = require('express');
 const express = require('express');
-const router = express.Router();
+const taskRouter = express.Router();
 
+// DB CONNECTION
 const pg = require('pg');
+const { mainModule } = require('process');
 const Pool = pg.Pool;
 const pool = new Pool({
-  database: 'task_list',
-  host: 'localhost',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000
+  database: 'task_list', // name of database
+  host: 'localhost', // database server
+  port: 5432, // Postgres default
+  max: 10, // max queries at once
+  idleTimeoutMillis: 30000 // 30 seconds to try to connect before cancelling query
 });
 
 // not required, but useful for debugging:
@@ -19,5 +21,20 @@ pool.on('connect', () => {
 
 pool.on('error', (error) => {
   console.log('error in postgres pool.', error);
-})
+});
 
+taskRouter.get('/', (req,res) =>{
+  let queryText = `SELECT * FROM "tasks";`;
+
+  pool.query(queryText).then((response) =>{
+    console.log('get successful');
+    res.send(response);
+  }).catch((error) => {
+    alert('error GETting', error);
+    res.sendStatus(500);
+  });
+});
+
+
+
+module.exports = taskRouter;

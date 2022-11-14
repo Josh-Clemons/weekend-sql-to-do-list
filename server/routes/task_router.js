d806258434cc90cdf237c932cdb0c08df70322b9
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-14 15:48:01 -0600

    updated database settings for heroku

--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -4,16 +4,27 @@ const { get } = require('http');
 const taskRouter = express.Router();
 
 // DB CONNECTION
+let pool;
 const pg = require('pg');
 const { mainModule } = require('process');
 const Pool = pg.Pool;
-const pool = new Pool({
-  database: 'weekend-to-do-app',
-  host: 'localhost',
-  port: 5432,
-  max: 10,
-  idleTimeoutMillis: 30000 
-});
+
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else{
+      pool = new pg.Pool({
+      database: 'weekend-to-do-app',
+      host: 'localhost',
+      port: 5432,
+      max: 10,
+      idleTimeoutMillis: 30000 
+  });
+};
 
 // GET/SELECT all items from tasks table
 taskRouter.get('/:sortOrder', (req,res) =>{

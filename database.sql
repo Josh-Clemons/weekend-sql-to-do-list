2176e2b8562c2b47e983caf0039387ae7fd66451
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-14 10:57:22 -0600

    added additional SQL data

--- database.sql
+++ database.sql
@@ -3,8 +3,11 @@ CREATE TABLE tasks (
     "owner" varchar(12) NOT NULL,
     "date" date NOT NULL,
     "details" varchar(800) NOT NULL,
-    "is_complete" boolean
+    "is_complete" boolean DEFAULT FALSE,
+    "completed_on" date
 );
 
 INSERT INTO "tasks" ("owner", date, details, is_complete)
-VALUES ('Josh', '02-17-2023', 'Be a primer', 'FALSE');
+VALUES ('Josh', '02-17-2023', 'Be a primer', 'FALSE'), 
+    ('Josh', '02-28-2023', 'Have a huge b-day party', 'FALSE'), 
+    ('Josh', '04-01-2023', 'Have a programming job by this day', 'FALSE');
